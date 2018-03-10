3e764c04122831e7c2f8e001f8eda72ed485dad1
Cédric Picard <cedric.picard@openmailbox.net>
2018-03-11 00:31:19 +0100

    More useful user interface

--- source/app.d
+++ source/app.d
@@ -7,16 +7,36 @@ import std.range;
 import std.stdio;
 import std.string;
 
+immutable help_text = `crunchyroll - watch animes without pain
+
+Usage: crunchyroll -h
+       crunchyroll add TITLE
+       crunchyroll see TITLE
+       crunchyroll remove TITLE
+       crunchyroll status [[TITLE] NEW_STATUS]
+
+Options:
+    -h, --help  Print this help and exit
+
+Commands:
+    add         Add or search for animes
+    see         See the next episode of TITLE
+    remove      Remove an anime by TITLE
+    status      See or set the current status of all or one animes
+
+Arguments:
+    URL         An crunchyroll anime URL
+    TITLE       Part of an anime title.
+                Case insensitive, the first title to match is taken.
+    NEW_STATUS  Integer, number of the episode in the serie`;
+
 struct AnimeRecord {
     string name;
     string link;
-}
 
-auto search(RecordRange)(RecordRange records, string query) {
-    return records
-             .filter!(r => r.name
-                            .toLower
-                            .canFind(query.toLower));
+    bool match(string query) {
+        return name.toLower.canFind(query.toLower);
+    }
 }
 
 auto episodeList(AnimeRecord record) {
@@ -43,7 +63,7 @@ auto recordList(string animeList) {
                     .front;
 }
 
-auto loadSeen(string path) {
+auto loadDb(string path) {
     import std.file: exists;
     import asdf: parseJsonByLine, deserialize;
 
@@ -63,56 +83,41 @@ auto loadSeen(string path) {
     return result;
 }
 
-void saveSeen(int[AnimeRecord] seen, string path) {
+void saveDb(int[AnimeRecord] db, string path) {
     import asdf: serializeToJson;
 
     File(path, "w+").write(
-        seen.byKeyValue
+        db.byKeyValue
             .array
             .serializeToJson
             .replace("\"key\"",   "\"anime\"")
             .replace("\"value\"", "\"seen\""));
 }
 
-int main(string[] args) {
-    import std.process: execute;
+int cmdAdd(int[AnimeRecord] db, string title) {
     import std.net.curl: byLine;
-
-    if (args.length == 1) {
-        writeln("Usage: crunchyroll NAME");
-        return 1;
-    }
-
-    immutable savePath = "/home/cym13/.local/share/crunchyroll/seen.json";
 
     immutable animeListUrl = "http://www.crunchyroll.com/ajax/"
                            ~ "?req=RpcApiSearch_GetSearchCandidates";
 
-    auto animeList = animeListUrl.byLine.dropOne.front.to!string;
-
-    auto seen = loadSeen(savePath);
-    scope(success) saveSeen(seen, savePath);
-
-    auto recordSearch = animeList.recordList.search(args[1]).array;
+    auto recordSearch = animeListUrl.byLine
+                                    .dropOne
+                                    .front
+                                    .to!string
+                                    .recordList
+                                    .filter!(r => r.match(title))
+                                    .array;
 
     AnimeRecord record;
     if (recordSearch.length == 1) {
         record = recordSearch[0];
     }
     else {
-        writeln("* Recently seen:");
-
-        seen.byKey
-            .array
-            .retro
-            .take(5)
-            .each!(a => writeln("\t", a.name, " (", seen[a], ")"));
-
         writeln("\n* Found ", recordSearch.length, " matching animes.\n");
 
         recordSearch.enumerate.each!((l, r) => writeln(l, "\t", r.name));
 
-        write("\n* Which one do you want to see?  ");
+        write("\n* Which one do you want to add?  ");
 
         uint index;
 
@@ -132,15 +137,46 @@ int main(string[] args) {
         record = recordSearch[index];
     }
 
-    if (record !in seen) {
-        seen[record] = 0;
+    db[record] = 0;
+
+    return 0;
+}
+
+void cmdRemove(int[AnimeRecord] db, string title) {
+    db.keys
+      .filter!(r => r.match(title))
+      .each!(r => db.remove(r));
+}
+
+void cmdStatus(int[AnimeRecord] db, string title="", int newStatus=-1) {
+    if (newStatus < 0) {
+        db.byKeyValue
+          .filter!(kv => kv.key.match(title))
+          .each!(kv => writefln("%s [%d]", kv.key.name, kv.value));
+    }
+    else {
+        db.keys
+          .filter!(r => r.match(title))
+          .each!(r => db[r] = newStatus);
+    }
+}
+
+int cmdSee(int[AnimeRecord] db, string title) {
+    import std.process: execute;
+
+    auto record = db.keys
+                    .filter!(r => r.match(title))
+                    .front;
+
+    if (record !in db) {
+        db[record] = 0;
     }
 
     auto toSee = record.episodeList
-                       .drop(seen[record])
+                       .drop(db[record])
                        .takeOne;
 
-    writeln("* Playing episode ", seen[record] + 1,
+    writeln("* Playing episode ", db[record] + 1,
             " from ", record.name,
             ": " ~ toSee.front);
 
@@ -152,8 +188,46 @@ int main(string[] args) {
     immutable play = execute(["see", "-f", toSee.front]);
 
     if (play.status == 0) {
-        seen[record]++;
+        db[record]++;
     }
 
     return 0;
+}
+
+int main(string[] args) {
+    if (args.canFind("-h")) {
+        writeln(help_text);
+        return 0;
+    }
+
+    immutable savePath = "/home/cym13/.local/share/crunchyroll/db.json";
+    auto db = loadDb(savePath);
+    scope(success) saveDb(db, savePath);
+
+    if (args.length < 2 || args[1] == "status") {
+        switch (args.length) {
+            case 1:
+            case 2:  cmdStatus(db, "");
+                     break;
+            case 3:  cmdStatus(db, args[2]);
+                     break;
+            default: cmdStatus(db, args[2], args[3].to!int);
+                     break;
+        }
+        return 0;
+    }
+    else if (args[1] == "remove") {
+        cmdRemove(db, args[2]);
+        return 0;
+    }
+    else if (args[1] == "see") {
+        return cmdSee(db, args[2]);
+    }
+    else if (args[1] == "add") {
+        return cmdAdd(db, args[2]);
+    }
+    else {
+        writeln(help_text);
+        return 1;
+    }
 }
